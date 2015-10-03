5f564591aad09fcd96f681a375680c6f2c074450
doronsadeh <doron.sadeh.dev@gmail.com>
2015-10-03 20:35:38 +0300

    Optimized erase function
    Removed loops over articles, going directly to authors, then climb to
    articles.
    Removed redundant querySelectAll.

--- ext/src/js/publishers/shoken/shoken.js
+++ ext/src/js/publishers/shoken/shoken.js
@@ -50,9 +50,7 @@ var Shoken = function(tracker) {
 										 "קרולינה לנדסמן":"carolina-landsman",
 										 "צפי סער":"tzafi-saar"};
 
-	this.articleSelectors = ['article','article.teaser--c','article.article','article.mh__teaser','article.h-mb','article.card','article.media','ul>li','ul>li.g__cell','article.g__cell','article.teaser','div.g__cell>article'];
-	
-	this.authorSelectors = ['div>div>div>div>div>div.t-byline>address','a>h3[class*="teaser"]>span','a>div.media__content>div.t-byline>address','a>header>p.pic__caption','a>address.t-address','header>div>div>div.t-byline>address>a[rel="author"]','a>div.media__content>p.t-address','a>div>div.t-byline>address','a>h3>span>span.t-kicker','section>div.t-byline>address','a>address.t-address','a>div.t-byline>address','a>div>div.t-byline>address','div.t-byline>address','a>article.media>div.media__content>h3>div.t-epsilon'];
+	this.authorSelectors = ['address','a>h3[class*="teaser"]>span','a>header>p.pic__caption','header>div>div>div.t-byline>address>a[rel="author"]','a>div.media__content>p.t-address','a>h3>span>span.t-kicker','a>article.media>div.media__content>h3>div.t-epsilon'];
 
 	// Create a uniform name list of authors for tracking
 	var dpKeyList = Object.keys(this.authorsNormalizedXlatTable);
@@ -107,6 +105,87 @@ Shoken.prototype._climbeToArticle = function(element) {
 	return null;
 };
 
+
+Shoken.prototype._erase = function(authorsMap) {
+	
+	// For tracking
+	var blockedAuthorsDict = {};
+
+	this.modified = 0;
+
+	var authorz = document.querySelectorAll(this.authorSelectors);
+	
+	for (var z = 0; z < authorz.length; z++) {
+		var author = authorz[z].firstChild;
+
+		var actualAuthorString = '';
+		if (typeof author.data != 'undefined') {
+			actualAuthorString = author.data;
+		}
+		else if (typeof author.firstChild != 'undefined' && typeof author.firstChild.data != 'undefined') {
+			actualAuthorString = author.firstChild.data;
+		}
+
+		for (var a = 0; a < this.authorsRegEx.length; a++) {
+
+			var candidates = this.authorsRegEx[a].exec(actualAuthorString);
+			for (var y = 0; candidates !== null && y < candidates.length; y++) {
+
+				var candidate = candidates[y];
+
+				var articleToHide = this._climbeToArticle(authorz[z]);
+
+				var toHide = this._isHide(authorsMap, this.authorsNormalizedXlatTable, candidate);
+				if (toHide && this.authorsRegEx[a].test(candidate)) {
+					// Hide only if not already hidden
+					if (articleToHide !== null) {
+						if (articleToHide.style.display !== 'none') {
+							articleToHide.style.setProperty('display', 'none', 'important');
+						}
+
+						articleToHide.setAttribute('data-zenreader-hide-article','true');
+						authorz[z].setAttribute('data-zenreader-hide-author',candidate);
+
+						var k = this._normalizeAuthor(this.authorsNormalizedXlatTable, candidate);
+						if (k) {
+							blockedAuthorsDict[k] = 1;
+						}
+					}
+				}
+				else if (!toHide && this.authorsRegEx[a].test(candidate)) {
+					articleToHide.style.setProperty('display', '', '');
+					articleToHide.removeAttribute('data-zenreader-hide-article');
+
+					var q = this._normalizeAuthor(this.authorsNormalizedXlatTable, candidate);
+					if (q) {
+						blockedAuthorsDict[q] = 0;
+					}
+				}
+			}
+		}
+	}
+	
+	this.modified = document.querySelectorAll('[data-zenreader-hide-article]').length;
+
+	if (this.modified !== this.copyModified) {
+		this.copyModified = this.modified;
+		console.log('sending modified: ', this.copyModified);
+		chrome.runtime.sendMessage({incr: this.copyModified}, function(response) {
+		});
+
+		var kl = Object.keys(this.authorsTrackingUniformName);
+		for (var r = 0; r < kl.length; r++) {
+			if (blockedAuthorsDict[this.authorsTrackingUniformName[kl[r]]] > 0) {
+				this.tracker.sendEvent('BlockedAuthorOnPage', this.authorsTrackingUniformName[kl[r]], blockedAuthorsDict[this.authorsTrackingUniformName[kl[r]]]);
+			}
+		}
+	}
+
+	this.modified = 0;
+	
+}
+
+/*
 Shoken.prototype._erase = function(authorsMap) {
 	// For tracking
 	var blockedAuthorsDict = {};
@@ -126,7 +205,8 @@ Shoken.prototype._erase = function(authorsMap) {
 					// Select exactly under
 					specificAddressSelectors = articleCardCSSPAth + '>' + this.authorSelectors[u];
 
-					var authorz = document.querySelectorAll(specificAddressSelectors);
+					// @ PERF!
+					var authorz = document.querySelector(specificAddressSelectors);
 
 					for (var z = 0; z < authorz.length; z++) {
 						var author = authorz[z].firstChild;
@@ -204,6 +284,7 @@ Shoken.prototype._erase = function(authorsMap) {
 	this.modified = 0;
 
 };
+*/
 
 Shoken.prototype._hideTalkbacks = function() {
 	var self = publisherInstances["Shoken"];
