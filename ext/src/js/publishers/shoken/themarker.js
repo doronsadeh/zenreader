78df412be4337e2ec1b043a6c418a385e247b6e1
doronsadeh <doron.sadeh.dev@gmail.com>
2015-10-25 10:31:49 +0200

    UI fixes in no-synopsis

--- ext/src/js/publishers/shoken/themarker.js
+++ ext/src/js/publishers/shoken/themarker.js
@@ -110,36 +110,63 @@ TheMarker.prototype._synopsis = function() {
                                 document.removeChild(existingSyn);
                             }
 
+                            var articleFirstParag = document.querySelector('section.article__entry>p.t-body-text');
+
+                            // Place visual queue while computing synopsis
+                            var pendingDiv = document.createElement('DIV');
+                            pendingDiv.id = 'zen-pending-review';
+                            pendingDiv.style.width = '100%';
+                            pendingDiv.style.textAlign = 'center';
+        
+                            var pendingGIF = document.createElement('IMG');
+                            pendingGIF.src = 'https://raw.githubusercontent.com/doronsadeh/media/master/zenreader/loading-animated.gif';
+                            pendingGIF.style.width = '32px';
+                            pendingGIF.style.height = '32px';
+                            pendingGIF.id = 'zen-review-pending-GIF';
+                            pendingGIF.title = 'Zen Reader בונה תקציר למאמר';
+
+                            pendingDiv.appendChild(pendingGIF);
+        
+                            articleFirstParag.parentElement.insertBefore(pendingDiv, articleFirstParag);
+        
                             var synopsis = self._computeSynopsis(self, 'section.article__entry>p.t-body-text');
 
-                            if (null !== synopsis) {
-                                var articleFirstParag = document.querySelector('section.article__entry>p.t-body-text');
-
-                                var logo = document.createElement('IMG');
-                                logo.src = 'https://raw.githubusercontent.com/doronsadeh/media/master/zenreader/icon48.png';
-                                logo.style.width = '32px';
-                                logo.style.height = 'auto';
-                                logo.style.margin = '5px 5px 5px 15px';
-
-                                var logoSpan = document.createElement('SPAN');
-                                logoSpan.appendChild(logo);
-                                logoSpan.style.float = 'right';
-
-                                var sChild = document.createElement("P");
-
-                                sChild.appendChild(logoSpan);
-
-                                sChild.innerHTML += synopsis;
-                                sChild.style.backgroundColor = 'rgba(0,255,0,0.25)';
-                                sChild.style.fontSize = '90%';
-                                sChild.id = "zen-reader-synopsis";
-                                sChild.classList.add('t-body-text');
-                                sChild.style.padding = '15px';
-                                sChild.style.marginBottom = '50px';
-
-                                // Put it all together
-                                articleFirstParag.parentElement.insertBefore(sChild, articleFirstParag);
+                            var bgColor = 'rgba(0,255,0,0.25)';
+                            var marginB = '75px';
+                            var paddingB = '15px 15px 15px 15px';
+                            if (null === synopsis) {
+                                bgColor = 'rgba(180,180,180,0.25)';
+                                marginB = '10px';
+                                var paddingB = '15px 15px 25px 15px';
+                                synopsis = "<div>&#1492;&#1502;&#1506;&#1512;&#1499;&#1514; &#1492;&#1495;&#1500;&#1497;&#1496;&#1492; &#1500;&#1488; &#1500;&#1492;&#1510;&#1497;&#1490; &#1505;&#1497;&#1499;&#1493;&#1501; &#1500;&#1502;&#1488;&#1502;&#1512; &#1494;&#1492;. &#1497;&#1514;&#1499;&#1503; &#1513;&#1492;&#1502;&#1488;&#1502;&#1512; &#1511;&#1510;&#1512; &#1493;&#1502;&#1502;&#1510;&#1492;, &#1488;&#1493; &#1513;&#1500;&#1488; &#1504;&#1497;&#1514;&#1503; &#1492;&#1497;&#1492; &#1500;&#1492;&#1508;&#1497;&#1511; &#1505;&#1497;&#1499;&#1493;&#1501; &#1488;&#1497;&#1499;&#1493;&#1514;&#1497; &#1491;&#1497;&#1493;.</div>";
                             }
+        
+                            var logo = document.createElement('IMG');
+                            logo.src = 'https://raw.githubusercontent.com/doronsadeh/media/master/zenreader/icon48.png';
+                            logo.style.width = '32px';
+                            logo.style.height = 'auto';
+                            logo.style.margin = '5px 5px 5px 15px';
+
+                            var logoSpan = document.createElement('SPAN');
+                            logoSpan.appendChild(logo);
+                            logoSpan.style.float = 'right';
+
+                            var sChild = document.createElement("P");
+
+                            sChild.appendChild(logoSpan);
+
+                            sChild.innerHTML += synopsis;
+                            sChild.style.backgroundColor = bgColor;
+                            sChild.style.fontSize = '90%';
+                            sChild.id = "zen-reader-synopsis";
+                            sChild.classList.add('t-body-text');
+                            sChild.style.padding = paddingB;
+                            sChild.style.marginBottom = marginB;
+        
+                            // Put it all together
+                            pendingDiv.removeChild(document.getElementById('zen-review-pending-GIF'));
+                            pendingDiv.style.textAlign = '';
+                            pendingDiv.appendChild(sChild);
     });
 };
 
